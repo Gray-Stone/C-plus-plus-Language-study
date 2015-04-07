e2698b06904230a8feb99602c67e58f2a04f5756
Gray-Stone <leograyc@gmail.com>
2015-04-07 12:39:18 -0400

    a towns of bug

--- final/student/student.cpp
+++ final/student/student.cpp
@@ -1,11 +1,14 @@
 #include<vector>
 #include<list>
+#include<utility>
 #include<string>
 #include"course.cpp"
 
 using std::string;
 using std::vector;
 using std::list;
+using std::pair;
+using std::make_pair;
 
 
 
@@ -16,7 +19,9 @@ public:
 	string getname() {return name;}
 	void setname(string a){name=a;}
 
-	list<course>::iterator add_subject(string course_name, course::times time) // add a subject with only name and time
+
+////
+	pair< list<course>::iterator,bool > add_subject(string course_name, course::times time) // add a subject with only name and time
 	{
 		list<course>::iterator iter;
 		for (iter= subject.begin();iter!=subject.end();iter++)
@@ -32,28 +37,43 @@ public:
 			{
 			}
 			if(time.check_time() == iter->checktime())
-				return NULL;	//if there are same course in same time. there will be a error
+				return make_pair(course(),false);	//if there are same course in same time. there will be a error
 		}
 
-		course temp(course_name ,time) ;
-		subject.insert(iter,temp);
+//		course temp(course_name ,time) ;
+		subject.insert(iter,course(course_name ,time));
 		--iter;
-		return iter;
+		return make_pair(iter,true);
 	}
+////
 
-	list<course>::iterator add_subject(string course_name, course::times time ,int mgrade)	// add a subject with name time and midtrem
+	pair< list<course>::iterator,bool > add_subject(string course_name, course::times time ,int mgrade)	// add a subject with name time and midtrem
 	{
+		pair< list<course>::iterator , bool > temp;
 		list<course>::iterator iter;
-		iter=add_subject(course_name,time);
+		temp =add_subject(course_name,time);
+
+		if (temp.second)
+			return make_pair(iter,false);
+		else
+		{
         iter->setmgrade(mgrade);
-        return iter;
+        return make_pair(iter,true);
+		}
 	}
 
-	int add_subject(string course_name, course::times time , int mgrade,int fgrade)	// add a subject with name time and final
+	pair< list<course>::iterator,bool > add_subject(string course_name, course::times time , int mgrade,int fgrade)	// add a subject with name time and final
 	{
+		pair< list<course>::iterator , bool > temp;
 		list<course>::iterator iter;
-		iter = add_subject(course_name,time,mgrade);
+		temp =add_subject(course_name,time);
+		if (temp.second)
+			return make_pair(course(),false);
+		else
+		{
          iter->setfgrade(fgrade);
+         return make_pair(iter,true);
+		}
 	}
 
 	course exist_course (string name,course::times _time)
